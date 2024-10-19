c85663b30810e695b0b67ba5b0e4868a6129e618
habibafaisal <habibafaisal8@gmail.com>
2024-10-19 14:46:44 +0500

    1.0.1

---
+++ cli.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { extractRoutesFromServer } from "./routeExtractor";
+
+const args = process.argv.slice(2);
+
+const serverFile = args[0];
+const collectionName = args[1] || "My Automated API Collection";
+if (!serverFile) {
+  console.error("Error: Please provide the path to your Express server file.");
+  process.exit(1);
+}
+extractRoutesFromServer({
+  serverFilePath: serverFile,
+  collectionName,
+});

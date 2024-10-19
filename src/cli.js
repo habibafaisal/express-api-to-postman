6d94bc9aa298117c4505cd544c886af58a7df846
habibafaisal <habibafaisal8@gmail.com>
2024-10-19 16:16:33 +0500

    1.0.3

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { extractRoutesFromServer } from "../routeExtractor";
+import { extractRoutesFromServer } from "../routeExtractor.js";
 
 const args = process.argv.slice(2);
 
